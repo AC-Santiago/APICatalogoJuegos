0f3912108e2ecbdbf399d4701d2c0bec72f05b9e
Santiago-AC <acostasantiago311124578@gmail.com>
2024-03-31 13:19:31 -0500

    Se cambio el viewsets a views

--- core/Juegos/Api/viewsets.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets, permissions
-from ..models import Juegos
-from ..Api.serializers import JuegosSerializer
-
-
-class JuegosViewSet(viewsets.ModelViewSet):
-    queryset = Juegos.objects.all()
-    permission_classes = [permissions.AllowAny]
-    serializer_class = JuegosSerializer
