54f0e878df940c4e0514720c00d78f86cd4948c8
Santiago-AC <acostasantiago311124578@gmail.com>
2024-04-25 15:06:39 -0500

    Se actualizo los requerimientos y se agrego la funcionalidad de paginacion

--- CatalogoJuegos/settings.py
+++ CatalogoJuegos/settings.py
@@ -148,3 +148,8 @@ cloudinary.config(
     api_secret=os.getenv("CLOUDINARY_API_SECRET"),
     secure=True,
 )
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 10,
+}
