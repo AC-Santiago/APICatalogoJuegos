3d7dfc1231704506b80200518b27c2997302d2ca
Santiago-AC <acostasantiago311124578@gmail.com>
2024-04-28 17:08:59 -0500

    Se cambio la forma de mostrar la api a solo json

--- CatalogoJuegos/settings.py
+++ CatalogoJuegos/settings.py
@@ -157,6 +157,9 @@ cloudinary.config(
 )
 
 REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 10,
     "DEFAULT_AUTHENTICATION_CLASSES": [
