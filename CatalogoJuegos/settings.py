0c210db6f5707cb2a01e5171f1625460a52dc60a
Santiago-AC <acostasantiago311124578@gmail.com>
2024-04-30 07:47:17 -0500

    Se reconecto con la base de datos remota

--- CatalogoJuegos/settings.py
+++ CatalogoJuegos/settings.py
@@ -95,23 +95,12 @@ WSGI_APPLICATION = "CatalogoJuegos.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASE_URL = os.getenv("DATABASE_URL")
-# if DATABASE_URL:
-#     DATABASES = {
-#         "default": dj_database_url.config(
-#             default=DATABASE_URL,
-#         )
-#     }
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "GameVault_db",
-        "USER": "postgres",
-        "PASSWORD": "Utadeo*2024",
-        "HOST": "localhost",
-        "PORT": "5432",
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+        )
     }
-}
 
 
 # Password validation
